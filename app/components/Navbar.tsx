9d5018b2d2eddc388961aeaea8e53c788271fc31
Khushil-21 <khushilshah2105@gmail.com>
2025-09-03 09:15:13 +0530

    Navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router";
+
+export default function Navbar() {
+	return (
+		<nav className="navbar">
+			<Link to="/">
+				<p className="text-gradient text-2xl font-bold">Resumind</p>
+			</Link>
+			<Link to="/upload" className="primary-button w-fit">Upload Resume</Link>
+		</nav>
+	);
+}
